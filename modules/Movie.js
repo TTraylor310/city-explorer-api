9d965cc67b1181c73162aedb6269e1e5e0845ef8
Tim Traylor <ttraylor83@gmail.com>
2022-08-29 08:04:21 -0700

    working lab 10, cache included

--- modules/Movie.js
+++ modules/Movie.js
@@ -2,24 +2,35 @@
 
 const axios = require('axios');
 
+let cache = {};
+
 async function getMovies(request, response, next) {
   let city = request.query.city;
   const url = `https://api.themoviedb.org/3/search/movie`;
 
   let params = {
     query: city,
-
     // page: 1,
     api_key: process.env.REACT_APP_MOVIEDB_API_KEY,
   };
+  const key = 'movie-' + city;
 
   try{
-    // console.log(url);
-    const movieR = await axios.get(url, {params});
-    // console.log(movieR);
-    const movieArr = movieR.data.results.map(val => new MovieInfo (val));
-    // console.log(movieArr);
-    response.status(200).send(movieArr);
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)){
+      console.log('Movie cache hit');
+      response.status(200).send(cache[key].data);
+
+    } else {
+      console.log('Movie cache miss');
+      const movieR = await axios.get(url, {params});
+      const movieArr = movieR.data.results.map(val => new MovieInfo (val));
+      cache[key] = {
+        data: movieArr,
+        timestamp: Date.now(),
+      };
+      response.status(200).send(movieArr);
+    }
   } catch (error) {
     response.status(500).send('server erro22');
   }
